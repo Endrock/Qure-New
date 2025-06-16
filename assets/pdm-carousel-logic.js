d85021347b1b0e7b270ee7c86be07014466bba80
Kevin Corrales <kevin.corrales@powerdigitalmarketinginc.com>
2025-06-16 12:11:49 -0500

    update image position

--- assets/pdm-carousel-logic.js
+++ assets/pdm-carousel-logic.js
@@ -9,22 +9,32 @@
  */
 
 document.addEventListener('DOMContentLoaded', function () {
-    const showCustomSlides = document.body.hasAttribute('data-pdm-activation-slider');
-    
-  
-    const customSlides = [
-      'neck-v2',
-      'microinfusion-v2',
-      'led-mask-v2'
-    ];
-  
+  const showCustomSlides = document.body.hasAttribute('data-pdm-activation-slider');
+
+  const customSlides = [
+    'neck-v2',
+    'microinfusion-v2',
+    'led-mask-v2'
+  ];
+
+  const carousel = document.querySelector('.swiper-products-carousel .swiper-wrapper');
+  const thumbs = document.querySelector('.swiper-products-thumbs__container .swiper-wrapper');
+
+  customSlides.forEach(slideKey => {
+    const slide = document.querySelector(`.swiper-products-carousel .swiper-slide[data-pdm-slider-test="${slideKey}"]`);
+    const thumb = document.querySelector(`.swiper-products-thumbs__container .swiper-slide[data-pdm-slider-test="${slideKey}"]`);
+
     if (!showCustomSlides) {
-      customSlides.forEach(slideKey => {
-        const slide = document.querySelector(`.swiper-products-carousel .swiper-slide[data-pdm-slider-test="${slideKey}"]`);
-        const thumb = document.querySelector(`.swiper-products-thumbs__container .swiper-slide[data-pdm-slider-test="${slideKey}"]`);
-  
-        slide?.remove();
-        thumb?.remove();
-      });
+      slide?.remove();
+      thumb?.remove();
+    } else {
+      if (slide && carousel) {
+        carousel.insertBefore(slide, carousel.firstChild);
+      }
+
+      if (thumb && thumbs) {
+        thumbs.insertBefore(thumb, thumbs.firstChild);
+      }
     }
   });
+});
