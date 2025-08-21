00b9a0b25206a03c5b214dc64940fbd517369117
speedup-guru <paul@speedup.guru>
2025-08-21 17:58:20 +0400

    update dataLayer in purchase form

--- assets/__app-purchase-form.js
+++ assets/__app-purchase-form.js
@@ -102,6 +102,17 @@ document.addEventListener('DOMContentLoaded', function() {
         clearPreorderBoxes();
         tooglePreorderBox(preorder, product_variant_id);
 
+        if(window.location.pathname == '/pages/microinfusion') {
+
+            let json = {
+                event: 'purchase-form',
+                url: window.location.pathname,
+                product_variant_id: product_variant_id
+            }
+            window.dataLayer.push(json);
+        }
+
+
         $('.' + __section + " .total_price").find(".regular_price").text($(this).find(".regular_price").text());
         $('.' + __section + " .total_price").find(".sale_price").text($(this).find(".sale_price:visible").text().trim());
         $('.' + __section + " .btn_value").text($(this).attr("data-per"));
