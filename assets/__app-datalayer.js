befe76d5f6225fc951e2bc36a5c1843a0e0dedcd
speedup-guru <paul@speedup.guru>
2025-09-08 17:31:30 +0400

    udpate

--- assets/__app-datalayer.js
+++ assets/__app-datalayer.js
@@ -1,14 +1,10 @@
 document.addEventListener('__app-datalayer.purchaseForm', (event) => {
-
-    console.log('>>> aaaaqqqq1 >>> purchase-form', event.detail);
-
     //skip default product_variant_id
     if(event.detail.event == 'purchase-form') {
         if(event.detail.product_category === undefined) return;
     }
 
     if(event.detail.url == "/pages/microinfusion" || event.detail.url == "/pages/qure-microinfusion-offer") {
-        console.log('>>> aaaaqqqq2 >>> purchase-form', event.detail);
         window.dataLayer.push(event.detail);
     }
 });
